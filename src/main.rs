6102ea0ff5618316a8abe55239077d1ac6d5244f
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-11-28 15:51:09 +0800

    +main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug,Clone, Copy)]
+enum E{
+    A,
+    B
+}
+struct Cont{
+    items: Vec<E>
+}
+impl Cont{
+    fn take(&self,g:Option<E>)->E{
+        g.unwrap_or_else(||self.func())
+    }
+    fn func(&self)->E{
+        let mut cntA=0;
+        let mut cntB=0;
+        for i in &self.items{
+            match i{
+                E::A=>cntA+=1,
+                E::B=>cntB+=1
+            }
+        }
+        if cntA>cntB{
+            E::A
+        }else{
+            E::B
+        }
+    }
+}
+fn main(){
+    let x=Cont{
+        items:vec![E::A,E::B,E::A,E::B,E::B]
+    };
+    let u=Some(E::A);
+    let r=x.take(u);
+    println!("{:?}",r);
+    let u =None;
+    let r=x.take(u);
+    println!("{:?}",r);
 }
