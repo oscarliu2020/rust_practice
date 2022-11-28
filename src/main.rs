93d44ff177aabb677ff43549f0128d5b37285f27
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-11-28 16:12:57 +0800

    practice

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,7 @@
-#[derive(Debug,Clone, Copy)]
-enum E{
-    A,
-    B
-}
-struct Cont{
-    items: Vec<E>
-}
-impl Cont{
-    fn take(&self,g:Option<E>)->E{
-        g.unwrap_or_else(||self.func())
-    }
-    fn func(&self)->E{
-        let mut cntA=0;
-        let mut cntB=0;
-        for i in &self.items{
-            match i{
-                E::A=>cntA+=1,
-                E::B=>cntB+=1
-            }
-        }
-        if cntA>cntB{
-            E::A
-        }else{
-            E::B
-        }
-    }
-}
-fn main(){
-    let x=Cont{
-        items:vec![E::A,E::B,E::A,E::B,E::B]
-    };
-    let u=Some(E::A);
-    let r=x.take(u);
-    println!("{:?}",r);
-    let u =None;
-    let r=x.take(u);
-    println!("{:?}",r);
+fn main() {
+    let  list = vec![1, 2, 3];
+    let func=|| println!("{:?}",list);
+    println!("{:?}",list);
+    func();
+    println!("{:?}",list);
 }
