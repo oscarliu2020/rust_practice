b04da02c377a6eb738b7a40bde36b239f2a16dc5
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-11-29 09:15:10 +0800

    iterator

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,10 @@
-use std::thread;
-use std::any::type_name;
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
-#[derive(Debug)]
-struct Rect{
-    h:u32,
-    w:u32
-}
-struct F<T>{
-    data:T,
-}
-trait Show{
-    fn show(&self);
-}
-impl<T> Show for F<T>{
-    fn show(&self){
-        println!("show");
-    }
-} 
-impl<T> Show for &F<T>
-{
-    fn show(&self){
-        println!("show2");
-    }
-}
-impl dyn Show{
-    fn test(&self){
-        println!("hello");
-    }
-}
-
-
 fn main() {
-    let mut list=vec![Rect{h:100,w:10},Rect{h:10,w:99},Rect{h:10,w:11}];
-    let mut cnt=0;
-    let  func=|r:&Rect| {r.h;cnt+=1;};
-    list.sort_by_key(func);
-    dbg!(&list,cnt);
+    let v1=vec![1,2,3];
+    let v2:Vec<_>=v1.iter().map(|x| x+1).collect();
+    for (i,x) in v1.iter().enumerate(){
+        println!("{i}: {x}");
+    }
+    for (i,x) in v2.iter().enumerate(){
+        println!("{i}: {x}");
+    }
 }
