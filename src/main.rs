3df5712da8033a711974c29ea19abc55b97eccca
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-11-30 19:57:50 +0800

    cycle ref

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,19 @@
+use List::{Cons,Nil};
+use std::rc::Rc;
+use std::cell::RefCell;
 #[derive(Debug)]
-enum List<T>{
-    Cons(Rc<RefCell<T>>,Rc<List<T>>),
-    NIL
+enum List{
+    Cons(i32,RefCell<Rc<List>>),
+    Nil,
 }
-struct MyBox<T>(T);
-impl<T> MyBox<T>{
-    fn new (x:T)->Self{
-        Self(x)
-    }
-}
-impl<T> Drop for MyBox<T>{
-    fn drop(&mut self) {
-        println!("smart pointer drop");
-    }
-}
-impl<T> Deref for MyBox<T>
-{
-    type Target = T;
-    fn deref(&self)->&Self::Target{
-        &self.0
-    }
-}
-use std::{ops::Deref, fmt::Display, rc::Rc, cell::RefCell, borrow::Borrow};
-
-use List::{Cons,NIL};
-fn hello(a:&str){
-    println!("hello, {}",a);
-}
-fn main() {
-    {
-        let x=5;
-        let y=MyBox::new(x);
-        assert_eq!(x,*y);
-
-    }
-    {
-        let m=MyBox::new(String::from("world"));
-        hello(&m);
-        drop(m);
-        let q=5;
-        println!("{q:?}");
-    }
-    
-    {
-        let x=Rc::new(RefCell::new(5));
-        let a=Rc::new(Cons(Rc::clone(&x), Rc::new(NIL)));
-        println!("{}",Rc::strong_count(&a));
-        println!("{:?}",a);
-        let b=Cons(Rc::new(RefCell::new(3)),Rc::clone(&a));
-        println!("{}",Rc::strong_count(&a));
-        println!("{:?}",b);
-        {
-            let c=Cons(Rc::new(RefCell::new(4)),Rc::clone(&a));
-            println!("{}",Rc::strong_count(&a));
-            println!("{:?}",c);
-        }
-        *(x.borrow_mut())+=100;
-        println!("{:?}",b);
-    }
-}
-#[cfg(test)]
-mod tests{
-    use std::cell::RefCell;
-
-    trait Msg{
-        fn send(&self,msg:&str);
-    }
-    struct Msgq{
-        sets:RefCell<Vec<String>>
-    }
-    impl Msgq{
-        fn new()->Self{
-            Self { sets: RefCell::new(vec![]) }
-        }
-
-    }
-    impl Msg for Msgq{
-        fn send(&self,msg:&str) {
-            self.sets.borrow_mut().push(msg.to_string());
+impl List{
+    fn tail(&self)->Option<&RefCell<Rc<List>>>{
+        match self{
+            Cons(_,item)=>Some(item),
+            Nil=>None,
         }
     }
-    #[test]
-    fn test(){
-        let x =Msgq::new();
-        x.send("q");
-    }
+}
+fn main(){
+
 }
