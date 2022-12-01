8a2733de8ab7d09766689c90ee3af834f4e8b80a
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-12-01 11:58:01 +0800

    mutex

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
 use std::collections::btree_map::ValuesMut;
 use std::{thread, time::Duration, vec};
 use std::sync::mpsc;
+use std::sync::Mutex;
 fn main(){
-    let (tx,rx)=mpsc::channel();
-    let tx1=tx.clone();
-    let hd=thread::spawn(move ||{
-        let vals=vec!["a".to_string(),"b".to_string(),"c".to_string()];
-        for val in vals
-        {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_millis(1000));
-        }
-        
-    });
-    let hd1=thread::spawn(move ||{
-        let vals=vec!["d".to_string(),"e".to_string(),"f".to_string()];
-        for val in vals{
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_millis(100));
-        }
-    });
-    for recv in rx.iter(){
-        println!("{recv}");
+    let m=Mutex::new(5);
+    {
+        let mut num=m.lock().unwrap();
+        *num=6;
     }
+    println!("m:{:?}",m);
 }
