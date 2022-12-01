dff8cde406bc89ae6635ede60282115382bc8efc
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-12-01 10:23:15 +0800

    thread

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time::Duration};
+use std::{thread::{self, sleep}, time::Duration};
 
 fn main(){
     let handle =thread::spawn(||{
@@ -7,5 +7,9 @@ fn main(){
             thread::sleep(Duration::from_millis(1));
         }
     });
+    for i in 1..10{
+        println!("from main:{}",i);
+        thread::sleep(Duration::from_millis(1));
+    }
     handle.join().unwrap();
 }
