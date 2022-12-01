f2c6708be25b8be6ba80c6228f9fa39c2048a323
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-12-01 11:48:16 +0800

    +mpsc

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{thread, time::Duration, vec};
 use std::sync::mpsc;
 fn main(){
     let (tx,rx)=mpsc::channel();
+    let tx1=tx.clone();
     let hd=thread::spawn(move ||{
         let vals=vec!["a".to_string(),"b".to_string(),"c".to_string()];
         for val in vals
@@ -11,6 +12,13 @@ fn main(){
             thread::sleep(Duration::from_millis(1000));
         }
         
+    });
+    let hd1=thread::spawn(move ||{
+        let vals=vec!["d".to_string(),"e".to_string(),"f".to_string()];
+        for val in vals{
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(100));
+        }
     });
     for recv in rx.iter(){
         println!("{recv}");
