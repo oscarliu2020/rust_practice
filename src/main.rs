0a40a729868e21975eb934d50f4382ea282c948a
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-12-01 10:41:10 +0800

    thread move closure

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use std::{thread::{self, sleep}, time::Duration};
+use std::{thread, time::Duration, vec};
 
 fn main(){
-    let handle =thread::spawn(||{
+    {
+        let handle =thread::spawn(||{
         for i in 1..10{
             println!("from thread:{}",i);
             thread::sleep(Duration::from_millis(1));
         }
-    });
-    for i in 1..10{
-        println!("from main:{}",i);
-        thread::sleep(Duration::from_millis(1));
+        });
+        for i in 1..5{
+            println!("from main:{}",i);
+            thread::sleep(Duration::from_millis(1));
+        }
+        handle.join().unwrap();
     }
-    handle.join().unwrap();
+    {
+        let v=vec![1,2,3];
+        let handle=thread::spawn( move ||{println!("{v:?}")});
+        handle.join().unwrap();
+    }
+    
 }
