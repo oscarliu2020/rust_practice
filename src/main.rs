815b569f35e73b0c3a30a974849e28634cf417c4
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2022-12-05 16:41:11 +0800

    arc mutex

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::collections::btree_map::ValuesMut;
-use std::{thread, time::Duration, vec};
-use std::sync::mpsc;
-use std::sync::Mutex;
+use std::{sync::{Mutex,Arc}, thread};
+use std::time::Duration;
+use std::rc::Rc;
 fn main(){
-    let m=Mutex::new(5);
-    {
-        let mut num=m.lock().unwrap();
-        *num=6;
+    let counter=Arc::new(Mutex::new(0));
+    let mut  handles=vec![];
+    for  _ in 0..10{
+        let counter=Arc::clone(&counter);
+        let handle=thread::spawn(move ||{
+            let mut  num=counter.lock().unwrap();
+            *num+=1;
+
+        });
+        handles.push(handle);
     }
-    println!("m:{:?}",m);
+    for h in handles{
+        h.join().unwrap();
+    }
+    println!("{}",*counter.lock().unwrap());
+    let q=vec![1,2,3];
+    let x:i32=q.iter().sum();
 }
